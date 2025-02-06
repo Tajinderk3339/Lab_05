0263af7dac3127d0e574587e5af923ebd90c0aea
TajinderK3339 <tajinderkumar001@algomau.ca>
2025-02-06 16:57:22 -0500

    clear Button added

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,21 @@
 
-var output=document.getElementById('output');
-
+// var output=document.getElementById('output');
+var div =document.createElement('div');
+div.className='out';
+div.id='output';
 function square(m){
     return m*m;
 }
 
 function clickBtn(){
     var text=document.getElementById('name').value;
-    output.textContent=`Your output is ${square(text)}`;
+    div.textContent=`Your output is ${square(text)}`;
+    document.body.append(div);
+    // output.textContent=`Your output is ${square(text)}`;
     
+}
+
+function clearBtn(){
+    document.getElementById('output').remove();
+    document.getElementById('name').value='';
 }
